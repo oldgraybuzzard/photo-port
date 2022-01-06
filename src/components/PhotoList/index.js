63acbd972bd35552d539f6d18cb50c58fd2fcb24
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2022-01-06 17:35:50 -0500

    Modal test passed

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -127,16 +127,19 @@ const PhotoList = ({ category }) => {
   const toggleModal = (image, i) => {
     //current photo
     setCurrentPhoto({...image, index: i});
-    setIsModalOpen(true);
+    setIsModalOpen(!isModalOpen);
   }
 
   return (
     <div>
-      {isModalOpen && <Modal currentPhoto={currentPhoto} />}
+      {isModalOpen && (
+        <Modal currentPhoto={currentPhoto} onClose={toggleModal} />
+      )}
+      
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
-            src={require(`../../assets/small/${category}/${i}.jpg`).default}
+            src={require(`../../assets/small/${category}/${i}.jpg`)}
             alt={image.name}
             className="img-thumbnail mx-1"
             onClick={() => toggleModal(image, i)}
